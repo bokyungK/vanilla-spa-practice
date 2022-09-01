19f12cc9961cf26d02a75146be31edf1ab4df4b1
freebob2b <freebob2b@gmail.com>
2022-09-01 18:43:17 +0900

    fix problem from prev and next button on browser

--- router/history.js
+++ router/history.js
@@ -3,7 +3,7 @@ const app = document.querySelector('.app');
 const nav = document.createElement('nav');
 const ul = document.createElement('ul');
 const div = document.createElement('div');
-div.className = 'content-box';
+div.className = 'title-box';
 
 const pages = ['Home', 'About', 'Project'];
 
@@ -20,19 +20,23 @@ app.appendChild(nav); // 네비게이션 렌더링
 app.appendChild(div); // 콘텐츠 표시 영역 렌더링
 
 // 라우팅
+    // 1. 네비게이션이 눌렸을 때 경로를 바꾸고, 경로에 맞게 렌더링
 const historyLinker = document.querySelectorAll('.history');
 
 historyLinker.forEach((element) => {
     element.addEventListener('click', (event) => {
         const pathName = event.target.getAttribute('route');
         window.history.pushState({}, pathName, window.location.origin + pathName);
-        renderHTML(element, pathName)
+        renderHTML(pathName);
     })
 })
+    // 2. 뒤로가기, 앞으로 가기로는 렌더링이 바뀌지 않는 문제 처리 (주소만 바뀌어도 렌더링이 바뀌어야 함)
+window.onpopstate = () => {
+    renderHTML(window.location.pathname);
+}
 
-// 경로별 렌더링
-const renderHTML = (element) => {
-    const title = element.innerHTML;
-    div.innerHTML = `${title} 경로의 페이지 입니다!`;
+// 경로별 렌더링을 처리하는 함수
+const renderHTML = (pathName) => {
+    div.innerHTML = `${pathName} 경로의 페이지 입니다!`;
     app.appendChild(div);
 }
